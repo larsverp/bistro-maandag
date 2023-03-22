72e30b4ce08d925569d1b7fa49837db807a42789
Lars van Erp <lars.vanerp@cm.com>
2023-03-22 13:45:41 +0100

    Updated the html

--- index.php
+++ index.php
@@ -12,7 +12,47 @@ $formatter = new IntlDateFormatter(
 $today = $formatter->format(new DateTime());
 
 if ($today === "maandag") {
-    echo "Ai! Ja het is vandaag bistro maandag";
+    $title = 'Ja 😢';
+    $image = 'sad_guy.svg';
+    $text = 'Het is vandaag bistro-maandag!';
 } else {
-    echo "Nee! Het is vandaag snack-" . $today . ".";
+    $title = 'Nee 🥳';
+    $image = 'bbq_guy.svg';
+    $text = 'Het is vandaag snack-'.$today;
 }
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ishetbistromaandag.nl</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background-color: #f2f2f2;
+            padding: 50px;
+        }
+        h1 {
+        font-size: 2.5em;
+        color: #333;
+        }
+        p {
+            font-size: 1.5em;
+            color: #555;
+        }
+        img {
+            max-width: 100%;
+            height: auto;
+            margin: 30px 0;
+        }
+    </style>
+</head>
+    <body>
+        <h1><?php echo $title; ?></h1>
+        <img src="<?php echo $image; ?>" alt="BBQ Guy">
+        <p><?php echo $text; ?></p>
+    </body>
+</html>
